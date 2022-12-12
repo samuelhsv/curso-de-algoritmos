9d710667bc1e620b054cdbb645489b8b2597e9c4
Samuel Henrique <samuelhenpx@gmail.com>
2022-12-12 13:56:51 -0300

    operators

---
+++ operadores/logicos/testes.py
@@ -0,0 +1,11 @@
+a = 2
+b = 3
+c = 5
+
+# logico recebe um valor True, então ele pode ser uma condição True
+logico = a == b # Comparei dois valores e atribuí um valor lógico à variável logico
+print(f"É {logico}")
+
+
+# Saída:
+# É true
