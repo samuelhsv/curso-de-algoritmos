2f0dcc070bea97a5ccb3fcea03d3f516d504d4e7
Samuel Henrique <samuelhenpx@gmail.com>
2022-12-12 14:13:50 -0300

    exercises folder

---
+++ operadores/logicos e relacionais/testes.py
@@ -0,0 +1,13 @@
+a = 2
+b = 3
+c = 5
+
+# logico recebe um valor True, então ele pode ser uma condição True
+logico = a == b  # Comparei dois valores e atribuí um valor lógico à variável logico
+
+logico2 = a < b and c > b  # logico2 so pode ser true se as duas forem verdadeiras
+
+print(f"São {logico} e {logico2}")
+
+# Saída:
+# São False e True
