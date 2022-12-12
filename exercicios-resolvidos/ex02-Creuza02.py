0cbfbe5400a5ec61bf0917ade11477feab5ff439
Samuel Henrique <samuelhenpx@gmail.com>
2022-12-12 14:33:02 -0300

    ex02

---
+++ exercicios-resolvidos/ex02-Creuza02.py
@@ -0,0 +1,3 @@
+reais = int(input("Quantos reais eu tenho? R$"))
+dolar = reais / 2.22 # Quantidade de reais / valor do dolar = a quantidade de dolar disponível pela cotação 
+print(f'Posso ter U${dolar:.2f}')
