e2fbc5dcfaf9d2e1706be66e4464db50f70ea215
Samuel Henrique <samuelhenpx@gmail.com>
2022-12-13 16:09:59 -0300

    Create Fatorial

---
+++ estruturas de repeticao/Fatorial.py
@@ -0,0 +1,12 @@
+num = int(input('Digite um número: '))
+cont = num
+fat = 1
+
+while True: 
+    fat = fat * cont # 1° iteração: fat recebe o valor inicial do fatorial
+                  # agora fat vale num e os proximos valores são abaixo de num 
+    cont -= 1        # depois da subtração que o fatorial é calculado e cont vale -1 corrente
+    if cont == 0:
+        break
+
+print(f'O valor do fatorial de {num} é igual a {fat}')
