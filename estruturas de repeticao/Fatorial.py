251d0d2db2a492851b2754e0b970a151fb9473cd
Samuel Henrique <samuelhenpx@gmail.com>
2022-12-13 16:16:52 -0300

    Update Fatorial.py

--- estruturas de repeticao/Fatorial.py
+++ estruturas de repeticao/Fatorial.py
@@ -1,12 +1,19 @@
-num = int(input('Digite um número: '))
-cont = num
-fat = 1
+import os
 
-while True: 
-    fat = fat * cont # 1° iteração: fat recebe o valor inicial do fatorial
-                  # agora fat vale num e os proximos valores são abaixo de num 
-    cont -= 1        # depois da subtração que o fatorial é calculado e cont vale -1 corrente
-    if cont == 0:
+while True:    
+    os.system('cls')
+
+    num = int(input('Digite um número: '))
+    cont = num
+    fat = 1
+    r = ''
+    while True: 
+        fat = fat * cont # 1° iteração: fat recebe o valor inicial do fatorial
+                         # agora fat vale num e os proximos valores são abaixo de num 
+        cont -= 1        # depois da subtração que o fatorial é calculado e cont vale -1 corrente
+        if cont == 0:
+            break
+    print(f'O valor do fatorial de {num} é igual a {fat}')
+    r = input('Quer continuar? [S/N] ')
+    if r == 'N':
         break
-
-print(f'O valor do fatorial de {num} é igual a {fat}')
